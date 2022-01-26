37de4f19370c926ff774fdaed3d289d3cf35a74e
eric <howcrazys@gmail.com>
2022-01-26 10:16:07 +0800

    fix: 包起來避免身分切換有問題

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -23,28 +23,30 @@
                         已登入
                     @elseif($role === 'admin')
                         管理介面
-                        @foreach ($users as $user)
-                            <div class="accordion accordion-flush">
-                                <div class="accordion-item">
-                                    <h2 class="accordion-header" id="flush-headingOne">
-                                    <button class="accordion-button collapsed" type="button" data-bs-toggle="collapse" data-bs-target="#flush-collapse{{$user->id}}">
-                                        {{$user->name}} 的訂單
-                                    </button>
-                                    </h2>
-                                    <div id="flush-collapse{{$user->id}}" class="accordion-collapse collapse">
-                                        <div class="accordion-body">
-                                            <ul>
-                                                @foreach ($user->orders as $order)
-                                                    <li>
-                                                        {{$order->order_no}} - 訂購者 {{$order->name}} - 電話 {{$order->phone}} - email {{$order->email}}
-                                                    </li>
-                                                @endforeach
-                                            </ul>
+                        @if (isset($users))
+                            @foreach ($users as $user)
+                                <div class="accordion accordion-flush">
+                                    <div class="accordion-item">
+                                        <h2 class="accordion-header" id="flush-headingOne">
+                                        <button class="accordion-button collapsed" type="button" data-bs-toggle="collapse" data-bs-target="#flush-collapse{{$user->id}}">
+                                            {{$user->name}} 的訂單
+                                        </button>
+                                        </h2>
+                                        <div id="flush-collapse{{$user->id}}" class="accordion-collapse collapse">
+                                            <div class="accordion-body">
+                                                <ul>
+                                                    @foreach ($user->orders as $order)
+                                                        <li>
+                                                            {{$order->order_no}} - 訂購者 {{$order->name}} - 電話 {{$order->phone}} - email {{$order->email}}
+                                                        </li>
+                                                    @endforeach
+                                                </ul>
+                                            </div>
                                         </div>
                                     </div>
                                 </div>
-                            </div>
-                        @endforeach
+                            @endforeach
+                        @endif
 
                         <div class="accordion accordion-flush">
                             <div class="accordion-item">
@@ -56,11 +58,13 @@
                                 <div id="flush-collapsenouser" class="accordion-collapse collapse">
                                     <div class="accordion-body">
                                         <ul>
-                                            @foreach ($noUsers as $noUser)
-                                                <li>
-                                                    {{$noUser->order_no}} - 訂購者 {{$noUser->name}} - 電話 {{$noUser->phone}} - email {{$noUser->email}} - time {{$noUser->created_at}}
-                                                </li>
-                                            @endforeach
+                                            @if (isset($noUsers))
+                                                @foreach ($noUsers as $noUser)
+                                                    <li>
+                                                        {{$noUser->order_no}} - 訂購者 {{$noUser->name}} - 電話 {{$noUser->phone}} - email {{$noUser->email}} - time {{$noUser->created_at}}
+                                                    </li>
+                                                @endforeach
+                                            @endif
                                         </ul>
                                     </div>
                                 </div>
